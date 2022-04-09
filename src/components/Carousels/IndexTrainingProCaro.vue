172f48c843a4d834f6f4188622cc083054dfdca1
youpussycat <20242524@qq.com>
2022-04-09 14:02:26 +0800

    zhaao

---
+++ src/components/Carousels/IndexTrainingProCaro.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="carousel">
+		<a-carousel arrows dots-class="slick-dots slick-thumb">
+			<template #customPaging="props">
+				<form name="downButton">
+					<a>
+						<label :for="getImgUrl(props.i)"></label>
+						<input type="radio" :id="getImgUrl(props.i)" style="display: block;">
+					</a>
+				</form>
+			</template>
+			<div v-for="item in imgCont" :key="item">
+				<img :src="baseUrl + 'abstract0' + item + '.jpg'" alt="走马灯图片2"/>
+
+			</div>
+		</a-carousel>
+	</div>
+</template>
+<script>
+	import {Carousel} from 'ant-design-vue';
+	const baseUrl = 'https://raw.githubusercontent.com/vueComponent/ant-design-vue/main/components/carousel/demo/';
+	export default {
+		name:"IndexTrainingProCaro",
+		components:{
+			ACarousel:Carousel,
+		},
+		data() {
+			return {
+
+			};
+		},
+		setup() {
+			const getImgUrl = (i) => {
+				return `${baseUrl}abstract0${i + 1}.jpg`;
+			};
+			// eslint-disable-next-line no-unused-vars
+			let imgCont = 4;
+			return {
+				baseUrl,
+				imgCont,
+				getImgUrl,
+			};
+		},
+
+	}
+</script>
+<style scoped>
+	/* For demo */
+	.ant-carousel {
+		height: 100%;
+	}
+	.ant-carousel ::v-deep(.slick-slider),.ant-carousel ::v-deep(.slick-list),.ant-carousel ::v-deep(.slick-slide),.ant-carousel ::v-deep(.slick-track), .ant-carousel ::v-deep(.slick-track) div{
+		height: 100%;
+		width: 100%;
+	}
+	.carousel {
+		top:100px;
+		width: 100%;
+		height: 100%;
+	}
+	.ant-carousel ::v-deep(.slick-dots){
+		margin-right: 0;
+		margin-left: auto;
+		width: 10%;
+	}
+	.ant-carousel ::v-deep(.slick-active), .ant-carousel ::v-deep(li),.ant-carousel ::v-deep(.slick-dots li.slick-active){
+		width: auto;
+	}
+
+	.ant-carousel ::v-deep(.slick-slide img) {
+		height: 100%;
+		width: 100%;
+	}
+
+	.ant-carousel ::v-deep(.slick-next){
+		right:0;
+	}
+
+
+</style>
---
+++ src/components/HomePage/IndexTrainingPro.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="indexTrainingPro">
+
+		<index-training-pro-caro></index-training-pro-caro>
+		<p></p>
+	</div>
+</template>
+
+<script>
+	import IndexTrainingProCaro from "@/components/Carousels/IndexTrainingProCaro";
+	export default {
+		name: "IndexTrainingPro",
+		components: {
+			IndexTrainingProCaro,
+		}
+	}
+</script>
+
+<style scoped>
+	.indexTrainingPro{
+		width: 50%;
+		height: 100%;
+		position: absolute;
+	}
+</style>

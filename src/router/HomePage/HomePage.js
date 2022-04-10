43d24f90c3489002ef4f3b7c4d9dc8feb6e86183
jiucheng120 <sjx18536228249@163.com>
2022-04-10 20:55:24 +0800

    1

--- src/router/HomePage/HomePage.js
+++ src/router/HomePage/HomePage.js
@@ -1,19 +1,18 @@
 
 const ExampleCourses = ()=> import("../../components/HomePage/ExampleCourses");
 const IndexTrainingPro = () => import("../../components/HomePage/IndexTrainingPro")
-<<<<<<< HEAD
 const FamousTeacherStyle = () => import("../../components/HomePage/FamousTeacherStyle");
 const LegalNotices = () => import("../../components/HomePage/LegalNotices");
 const TrainingArounds = () => import("../../components/HomePage/TrainingArounds");
 const OnlineLearning = () => import("../../components/HomePage/OnlineLearning");
 const Page_Prompt = () => import("../../components/HomePage/Page_Prompt")
-
-=======
 const RightSidebar = () => import("../../components/HomePage/RightSidebar")
 const MenuBar = () => import("../../components/HomePage/MenuBar")
 const Footer = () => import("../../components/HomePage/Footer")
 const Button = () => import("../../components/HomePage/Button")
->>>>>>> 9417440ed0411652157b5157c7d03839273a1445
+const Banner = () => import("../../components/HomePage/Banner")
+const Teacher = () => import("../../components/HomePage/Teacher")
+
 let PATH="/HomePage"
 let __homePage= [
     {//规定名称为HelloWorld的组件的component、path
@@ -72,7 +71,17 @@ let __homePage= [
         path: PATH+'/Page_Prompt',
         name:'Page_Prompt',
         component:Page_Prompt
-    }
+    },
+    {
+        path: PATH+'/Banner',
+        name:'Banner',
+        component:Banner
+    },
+    {
+        path: PATH+'/Teacher',
+        name:'Teacher',
+        component:Teacher
+    },
 ]
 import {_elegantTeachers} from "./ElegantTeachers";
  __homePage=__homePage.concat(_elegantTeachers);

76eee5f29d76a1b9dc7156abc033d3ea6a554a72
jongshy <13133249092>
2022-04-12 15:35:56 +0800

    +

--- src/router/HomePage/HomePage.js
+++ src/router/HomePage/HomePage.js
@@ -1,16 +1,19 @@
 
 const ExampleCourses = ()=> import("../../components/HomePage/ExampleCourses");
 const IndexTrainingPro = () => import("../../components/HomePage/IndexTrainingPro")
-const FamousTeacherStyle = () => import("../../components/HomePage/FamousTeacherStyle");
-const LegalNotices = () => import("../../components/HomePage/LegalNotices");
-const TrainingArounds = () => import("../../components/HomePage/TrainingArounds");
-const OnlineLearning = () => import("../../components/HomePage/OnlineLearning");
-const Page_Prompt = () => import("../../components/HomePage/Page_Prompt")
 const RightSidebar = () => import("../../components/HomePage/RightSidebar")
 const MenuBar = () => import("../../components/HomePage/MenuBar")
 const Footer = () => import("../../components/HomePage/Footer")
 const Button = () => import("../../components/HomePage/Button")
-
+const Banner = () => import("../../components/HomePage/Banner")
+const FamousTeacherStyle = () => import("../../components/HomePage/FamousTeacherStyle");
+const TrainingArounds = () => import("../../components/HomePage/TrainingArounds");
+const OnlineLearning = () => import("../../components/HomePage/OnlineLearning");
+const Page_Prompt = () => import("../../components/HomePage/Page_Prompt")
+const LegalNotices = () => import("../../components/HomePage/LegalNotices");
+const NoticeAndFileList = () => import("../../components/HomePage/NoticeAndFileList")
+const PageHeaderGroup = () => import("../../components/HomePage/PageHeaderGroup")
+const Teacher = () => import("../../components/HomePage/Teacher")
 let PATH="/HomePage"
 let __homePage= [
     {//规定名称为HelloWorld的组件的component、path
@@ -69,7 +72,28 @@ let __homePage= [
         path: PATH+'/Page_Prompt',
         name:'Page_Prompt',
         component:Page_Prompt
+    },
+    {
+        path: PATH+'/Banner',
+        name:'Banner',
+        component:Banner
+    },
+    {
+        path:PATH+'/NoticeAndFileList',
+        name:'NoticeAndFileList',
+        component:NoticeAndFileList
+    },
+    {
+        path:PATH+'/PageHeaderGroup',
+        name:'PageHeaderGroup',
+        component:PageHeaderGroup
+    },
+    {
+        path:PATH+'/Teacher',
+        name:'Teacher',
+        component:Teacher
     }
+
 ]
 import {_elegantTeachers} from "./ElegantTeachers";
  __homePage=__homePage.concat(_elegantTeachers);

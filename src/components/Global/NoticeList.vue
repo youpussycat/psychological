97782b6e9d3f27ecb9e4a71152a82bea7e95d07d
forwhat461 <15735784129>
2022-04-14 20:58:59 +0800

    +

--- src/components/Global/NoticeList.vue
+++ src/components/Global/NoticeList.vue
@@ -1,17 +1,38 @@
 <template>
-	<a :href="it.url" class="AList" v-for="(it,index) in MesList" :key="index">
-		<a  class="AListItem">
-			<div class="itemDate">
-				<p class="dateDay">{{it.date.day}}</p>
-				<p class="dateYM">{{it.date.year}}-{{it.date.month}}</p>
+	<div id="background">
+		<div id="main">
+			<div id="title">
+				<h3 id="titleText">培训纪实</h3>
+				<div id="titleBottom"></div>
+				<p id="titleMore">更多</p>
+				<div id="titleArrow0"></div>
+				<div id="titleArrow1"></div>
 			</div>
-			<div class="itemDes">
-				<p class="AItemTitle">{{it.title}}</p>
-				<p class="AItemDes">{{it.des}}</p>
+			<div id="context">
+				<div class="contextImage">
+					<img class="contextImage">
+					<div id="contextImageList">
+						<img class="ctxImageUnit" v-for="i in [1,2,3,4]">
+					</div>
+				</div>
+				<div id="textContext">
+					<a :href="it.url" class="AList" v-for="(it,index) in MesList" :key="index">
+						<a class="AListItem">
+							<div class="itemDate">
+								<p class="dateDay">{{it.date.day}}</p>
+								<p class="dateYM">{{it.date.year}}-{{it.date.month}}</p>
+							</div>
+							<div class="itemDes">
+								<p class="AItemTitle">{{it.title}}</p>
+								<p class="AItemDes">{{it.des}}</p>
+							</div>
+						</a>
+					</a>
+				</div>
 			</div>
-
-		</a>
-	</a>
+		</div>
+		<img id="emptyIMG" style="display: none;" src="../../assets/img/logo.png"/>
+	</div>
 
 </template>
 
@@ -22,30 +43,18 @@
 			return {
 				MesList:[//列表数据
 					{//跳转页面链接，item的时间，标题以及描述
-						url: "#",
-						date: {
-							day:"10",
-							month:"01",
-							year:"2022"
-						},
+						url: "javascript:;",
+						date: {day:Math.floor(Math.random()*12)+1,month:"01",year:"2022"},
 						title: "中国心理卫生协会心理咨询师",
 						des: "中国心理卫生协会心理咨询师专业技能培训 2022年单独第一次综合考试时间安排第一..."
 					},{
-						url: "#",
-						date: {
-							day:"10",
-							month:"01",
-							year:"2022"
-						},
+						url: "javascript:;",
+						date: {day:Math.floor(Math.random()*12)+1,month:"01",year:"2022"},
 						title: "中国心理卫生协会心理咨询师",
 						des: "中国心理卫生协会心理咨询师专业技能培训 2022年单独第一次综合考试时间安排第一11111111111111111111111111111111111111111"
 					},{
-						url: "#",
-						date: {
-							day:"10",
-							month:"01",
-							year:"2022"
-						},
+						url: "javascript:;",
+						date: {day:Math.floor(Math.random()*12)+1,month:"01",year:"2022"},
 						title: "中国心理卫生协会心理咨询师",
 						des: "中国心理卫生协会心理咨询师专业技能培训 2022年单独第一次综合考试时间安排第一..."
 					}
@@ -100,5 +109,84 @@
 		overflow: hidden;
 		text-overflow: ellipsis;
 		width: 540px;
+	}
+
+
+	#background {
+		background-color: #F7F7F7;
+		height: 534px;
+	}
+
+	#main {
+		width:1200px;
+		margin: 0px auto;
+	}
+	#title{
+		width: 100%;
+		height: 82px;
+		position: relative;
+	}
+	#titleText{
+		margin: 0px;
+
+		top: 40px;
+		position: absolute;
+	}
+	#titleBottom{
+		background-color: #05327B;
+		width: 39px;
+		height: 2px;
+
+		bottom: 0px;
+		position: absolute;
+	}
+	#titleMore{
+		right: 34px;
+		top: 45px;
+		position: absolute;
+	}
+	#titleArrow0{
+		background-color: #809FD0;
+		width: 14px;
+		height: 2px;
+
+		right: 14px;
+		top: 56px;
+		position: absolute;
+	}
+	#titleArrow1{
+		background-color: #809FD0;
+		width: 7px;
+		height: 2px;
+		transform: rotate(45deg);
+
+		right: 14px;
+		top: 53px;
+		position: absolute;
+	}
+
+
+	#context{
+		top:10px;
+		position: relative;
+	}
+	.contextImage{
+		width: 646px;
+		height: 392px;
+		display: inline-block;
+		position: relative;
+	}
+	.ctxImageUnit{
+		width: 180px;
+		height: 98px;
+		display: table-cell;
+	}
+	#contextImageList{
+		position: absolute;
+		top: 0;right: 0;
+	}
+	#textContext{
+		float: right;
+		width: 500px;
 	}
 </style>

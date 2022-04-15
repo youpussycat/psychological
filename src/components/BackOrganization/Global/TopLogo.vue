d651f00888d037743aa0ff24f9957a809bfe7339
youpussycat <20242524@qq.com>
2022-04-15 18:55:58 +0800

    zhao

--- src/components/BackOrganization/Global/TopLogo.vue
+++ src/components/BackOrganization/Global/TopLogo.vue
@@ -1,20 +1,25 @@
 <template>
 	<div class="TopLogoTotal">
-		<img class="TopLogoImg" :src="logoSrc" alt="logo"/>
+		<div class="TopLogoLeft">
+			<img class="TopLogoImg" :src="logoSrc" alt="logo"/>
+			<p class="TopLogoTitle" >心博睿</p>
+		</div>
 		<div class="TopLogoRight">
 			<div class="MessNotice">
 				<a-badge :count="1000" :overflow-count="999">
 					<a-avatar shape="circle" size="large" :src='MesImage'/>
 				</a-badge>
+
 			</div>
 			<div class="User">
-
+				<a-avatar shape="circle" size="large" :src='UserHeadImg'/>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+
 	import { Badge, Avatar } from 'ant-design-vue';
 	export default {
 		name: "TopLogo",
@@ -25,8 +30,9 @@
 		},
 		data() {
 			return {
-				logoSrc: "",
-				MesImage: 'require("../../../assets/img/消息.png")',
+				logoSrc: require("../../../assets/img/logo.png"),
+				MesImage: require("../../../assets/img/消息.png"),
+				UserHeadImg:"",
 
 			};
 		},
@@ -35,5 +41,11 @@
 </script>
 
 <style scoped>
-
+	.TopLogoImg, .TopLogoTitle, .TopLogoLeft, .TopLogoRight, .MessNotice, .User {
+		display: inline-block;
+	}
+	.TopLogoImg {
+		width: 60px;
+		height: auto;
+	}
 </style>
--- src/router/BackOrganization/Global.js
+++ src/router/BackOrganization/Global.js
@@ -20,6 +20,7 @@ for (const i of ComponentAttribute)//遍历上面的组件数组，用于生成�
 		//import函数在webPack中无法只使用变量，只能字面量与常量结合使用或是只是用字面量,似乎插值语法也用不了.const是常变量
 		component: () => import("../../components/BackOrganization/Global/"+i)
 	});
+/*
 for (const i of SmallComponent)//遍历上面的组件数组，用于生成暴露组件所需要的属性
 	__global.push({
 		path: SmallRouterPath + i,
@@ -27,5 +28,5 @@ for (const i of SmallComponent)//遍历上面的组件数组，用于生成暴�
 		//import函数在webPack中无法只使用变量，只能字面量与常量结合使用或是只是用字面量,似乎插值语法也用不了.const是常变量
 		component: () => import("../../components/BackOrganization/Global/SmallComponents/"+i)
 	});
-
+*/
 

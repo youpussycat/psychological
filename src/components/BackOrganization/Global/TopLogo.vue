e6d313ec9f29b5fd77feeca875dbcf6cc4f84d5e
youpussycat <20242524@qq.com>
2022-04-15 18:27:27 +0800

    zhao

--- src/components/BackOrganization/Global/TopLogo.vue
+++ src/components/BackOrganization/Global/TopLogo.vue
@@ -3,7 +3,9 @@
 		<img class="TopLogoImg" :src="logoSrc" alt="logo"/>
 		<div class="TopLogoRight">
 			<div class="MessNotice">
-
+				<a-badge :count="1000" :overflow-count="999">
+					<a-avatar shape="circle" size="large" :src='MesImage'/>
+				</a-badge>
 			</div>
 			<div class="User">
 
@@ -13,14 +15,19 @@
 </template>
 
 <script>
+	import { Badge, Avatar } from 'ant-design-vue';
 	export default {
 		name: "TopLogo",
 		components:{
+			ABadge: Badge,
+			AAvatar: Avatar,
 
 		},
 		data() {
 			return {
-				logoSrc:""
+				logoSrc: "",
+				MesImage: 'require("../../../assets/img/消息.png")',
+
 			};
 		},
 

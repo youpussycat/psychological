6cd27dde6200a722d9311ad2ed2dd3d94c87916d
jiucheng120 <sjx18536228249@163.com>
2022-04-15 22:00:52 +0800

    1

--- src/components/HomePage/Video_Details.vue
+++ src/components/HomePage/Video_Details.vue
@@ -3,12 +3,12 @@
     <div>
       <video :src="video.src"></video>
       <div class="sidebar">
-        <div v-for="data in video_data">
+        <div v-for="data in video_data" style="font-size: 14px">
           <div v-if="data.type==='title'" style="margin: 20px 20px 20px 20px">{{data.context}}</div>
-          <div v-else style="padding: 20px 20px 20px 20px">
+          <div v-else style="padding: 20px 20px 20px 20px" class="hover">
             <div class="video_type">{{data.type1}}</div>
             <text>{{data.context}}</text>
-            <div style="padding-left: 30px; color: #777777">{{data.duration}}分钟</div>
+            <div class="duration">{{data.duration}}分钟</div>
           </div>
         </div>
       </div>
@@ -27,36 +27,47 @@
         <span class="size1">授课教师：{{banner.teacher}}</span>
       </div>
     </div>
-    <div>
-      <div style="float: right; width: 350px; font-size: 16px; padding: 30px 30px 30px 30px;">
-        {{course1.title}}
-        <div v-for="item in course1.data" style="padding: 10px 10px 10px 10px; height: 220px;">
-          <img :src="item.img" alt="" style="height: 180px;">
-          <div>{{item.name}}</div>
-          <div style="float: right; color: #FD1E1B">{{item.free}}</div>
-          <img :src="item.avatar" alt="" style="width: 20px;"/>
-          <text style="padding-bottom: 10px; font-size: 12px">主讲：{{item.teacher}}</text>
-        </div>
+    <div style="float: right; width: 350px; font-size: 16px; padding: 30px 30px 30px 30px; background: #FFFFFF;">
+      {{course1.title}}
+      <div v-for="item in course1.data" style="padding: 10px 10px 10px 10px; height: 220px;">
+        <img :src="item.img" alt="" style="height: 180px;">
+        <div>{{item.name}}</div>
+        <div style="float: right; color: #FD1E1B">{{item.free}}</div>
+        <img :src="item.avatar" alt="" style="width: 20px;"/>
+        <text style="padding-bottom: 10px; font-size: 12px">主讲：{{item.teacher}}</text>
       </div>
+    </div>
+    <div style="background: #FFFFFF;">
       <div style="padding: 20px 0 20px 0;">
         <span style="font-weight: 800; font-size: 16px; border-bottom: 2px solid #043278; padding-bottom: 7px;">{{course.title}}</span>
         <span style="color: #999999; font-size: 16px; padding-left: 50px;">{{course.title2}}</span>
       </div>
       <div>
-        <input type="text" value="课程介绍" disabled>
+        <input type="text" value=" 课程介绍" disabled>
       </div>
-      <div style="width: 460px; float: left;">
+      <div style="width: 870px; float: left;">
         <img :src="course.img" alt="">
-      </div>
-      <div>
-        <input type="text" value="目录" disabled>
-        <div v-for="data in course.context">
-          <div v-if="data.type==='title'" style="margin: 20px 20px 20px 20px; font-size: 16px; font-weight: 800;">{{data.context}}</div>
-          <div v-else style="padding: 20px 20px 20px 20px; font-size: 14px;">
-            <div>{{data.context}}</div>
-            <div style="float: left; padding-right: 10px; padding-top: 1px"><img src="../../assets/img/video.png" alt="" style="width: 18px"></div>
-            <div style="color: #777777">{{data.type1}} | 时长：{{data.duration}}分钟</div>
+        <div>
+          <input type="text" value=" 目录" disabled>
+          <div v-for="data in course.context">
+            <div v-if="data.type==='title'" style="margin: 20px 20px 20px 20px; font-size: 16px; font-weight: 800;">{{data.context}}</div>
+            <div v-else style="padding: 20px 20px 20px 20px; font-size: 14px;">
+              <div v-if="data.flag===1" style="float: right">
+                <button style="background: #043278; font-size: 10px; width: 40px; height: 20px;">
+                  已观看
+                </button>
+              </div>
+              <div v-else style="float: right">
+                <button style="background: #F5F5F5; color: #999999; font-size: 10px; width: 40px; height: 20px;">
+                  未观看
+                </button>
+              </div>
+              <div>{{data.context}}</div>
+              <div style="float: left; padding-right: 10px; padding-top: 1px"><img src="../../assets/img/video.png" alt="" style="width: 18px"></div>
+              <div style="color: #CCCCCC">{{data.type1}} | 时长：{{data.duration}}分钟</div>
+            </div>
           </div>
+          <div style="text-align: center; padding-top: 60px; padding-bottom: 24px; color: #CCCCCC">已到底部</div>
         </div>
       </div>
     </div>
@@ -82,13 +93,15 @@ export default {
           type: "context",
           context: "心理健康咨询师是怎样炼成的",
           type1: "录播",
-          duration: "18"
+          duration: "18",
+          src: require("../../assets/video/123.mp4"),
         },
         {
           type: "context",
           context: "心理健康咨询师是怎样炼成的",
           type1: "录播",
-          duration: "18"
+          duration: "18",
+          src: require("../../assets/video/123.mp4"),
         },
         {
           type: "title",
@@ -98,7 +111,29 @@ export default {
           type: "context",
           context: "心理健康咨询师是怎样炼成的",
           type1: "录播",
-          duration: "18"
+          duration: "18",
+          src: require("../../assets/video/123.mp4"),
+        },
+        {
+          type: "context",
+          context: "心理健康咨询师是怎样炼成的",
+          type1: "录播",
+          duration: "18",
+          src: require("../../assets/video/123.mp4"),
+        },
+        {
+          type: "context",
+          context: "心理健康咨询师是怎样炼成的",
+          type1: "录播",
+          duration: "18",
+          src: require("../../assets/video/123.mp4"),
+        },
+        {
+          type: "context",
+          context: "心理健康咨询师是怎样炼成的",
+          type1: "录播",
+          duration: "18",
+          src: require("../../assets/video/123.mp4"),
         },
       ],
       banner: {
@@ -123,18 +158,21 @@ export default {
             context: "培训课程第一节",
             type1: "录播",
             duration: "18",
+            flag: 1,
           },
           {
             type: "context",
             context: "培训课程第一节",
             type1: "录播",
             duration: "18",
+            flag: 0,
           },
           {
             type: "context",
             context: "培训课程第一节",
             type1: "录播",
             duration: "18",
+            flag: 0,
           }
         ]
       },
@@ -179,17 +217,28 @@ video {
 }
 .sidebar {
   width: 36%;
-  height: 489px;
+  max-height: 489px;
+  overflow-y: auto;
   float: right;
   background: #141414;
   color: #b5b5b5;
   font-size: 14pt;
 }
 .video_type {
-  margin: 0px 10px 10px 10px;
-  background: #23b7fc;
+  margin: 5px 10px 10px 10px;
+  width: 30px;
+  height: 17px;
+  background: #6D6D6D;
   color: #141414;
+  font-size: 12px;
   float: left;
+  text-align: center;
+}
+.video_type:hover {
+  background: #23b7fc;
+}
+.hover:hover {
+  color: #23b7fc;
 }
 .header {
   padding: 10px 10px 10px 10px;
@@ -227,9 +276,21 @@ button {
   padding-right: 16pt;
 }
 input {
-  width: 68%;
+  width: 870px;
   border: none;
-  height: 50px;
-  font-size: 20px;
+  height: 30px;
+  font-size: 14px;
+  color: #AAAAAA;
+}
+.banner {
+  background: #ffffff;
+}
+.duration {
+  padding-left: 30px;
+  color: #777777;
+  font-size: 12px;
+}
+.duration:hover {
+  color: #23b7fc;
 }
 </style>

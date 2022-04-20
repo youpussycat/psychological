89f786627b0d721402637e6d12ec70d7d28e64c5
jongshy <13133249092>
2022-04-20 12:27:52 +0800

    +

--- src/components/BackOrganization/Global/Test.vue
+++ src/components/BackOrganization/Global/Test.vue
@@ -2,11 +2,14 @@
   <div>
     <button @click="test">测试</button>
     <Dialog></Dialog>
+    <button @click="del">删除</button>
+    <Bubble v-if="bubble" title="确定删除吗？"/>
   </div>
 </template>
 
 <script>
 import Dialog from "@/components/BackOrganization/Global/Dialog";
+import Bubble from "@/components/BackOrganization/Global/Bubble";
 export default {
   name:'Test',
   data(){
@@ -16,18 +19,23 @@ export default {
         title:'添加用户',  //弹窗的标题
         add: true,  //是否是添加用户
         status: true, //switch开关
-        read: true,  //input 框是否只读，false：只读，true：开编辑
+        read: true,  //input 框是否只读，false：只读，true：可编辑
         done: true  //确认按钮
-      }
+      },
+      bubble: false,
     }
   },
   components:{
+    Bubble,
     Dialog
   },
   methods:{
     test(){
       this.$bus.emit('sendData', this.data);
       this.data.show = true;
+    },
+    del(){
+      this.bubble = true;
     }
   },
   mounted(){
@@ -48,9 +56,8 @@ export default {
       console.log('账户名称：', data.accountName);
       console.log('用户密码', data.password);
       console.log('备注：', data.remark);
-
-
-    })
+    });
+    // this.$bus.on('')
   }
 }
 </script>
